07ce58adb442f706def1977417823d7a0a5d3f94
Rexbase <gdnwstomw@gmail.com>
2023-07-12 07:40:22 -0700

    Updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,52 @@
 #include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - Read text file and print to STDOUT.
+ * @filename: Text file to be read.
+ * @letters: Number of letters to be read.
+ * Return: Number of bytes read and printed (w).
+ *         0 when the function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int i, y;
 	char *buf;
-	if (!filename)
+	ssize_t t, w;
+
+	if (filename == NULL)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
+	if (buf == NULL)
 	{
-		free(buf);
+		close(fd);
 		return (0);
 	}
-	buf[i] = '\0';
-	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
+
+	t = read(fd, buf, letters);
+	if (t == -1)
 	{
 		free(buf);
+		close(fd);
 		return (0);
 	}
+
+	w = write(STDOUT_FILENO, buf, t);
+	if (w == -1 || w != t)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
 	free(buf);
-	return (y);
+	close(fd);
+	return (w);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,53 +1,67 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments passed to the program
- * @argv: array of arguments
+ * main - Copies the content of a file to another file
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of arguments
  *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int fd_r, fd_w, x, m, n;
+	int fd_r, fd_w, bytes_read, bytes_written;
 	char buf[BUFSIZ];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		fprintf(stderr, "Usage: cp file_from file_to\n");
+		exit(EXIT_FAILURE);
 	}
+
 	fd_r = open(argv[1], O_RDONLY);
-	if (fd_r < 0)
+	if (fd_r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
+
 	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((x = read(fd_r, buf, BUFSIZ)) > 0)
+	if (fd_w == -1)
 	{
-		if (fd_w < 0 || write(fd_w, buf, x) != x)
+		fprintf(stderr, "Error: Can't write to file %s\n", argv[2]);
+		close(fd_r);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((bytes_read = read(fd_r, buf, BUFSIZ)) > 0)
+	{
+		bytes_written = write(fd_w, buf, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			fprintf(stderr, "Error: Can't write to file %s\n", argv[2]);
 			close(fd_r);
-			exit(99);
+			close(fd_w);
+			exit(EXIT_FAILURE);
 		}
 	}
-	if (x < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	m = close(fd_r);
-	n = close(fd_w);
-	if (m < 0 || n < 0)
-	{
-		if (m < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
-		if (n < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
-		exit(100);
-	}
-	return (0);
-}
 
+	if (bytes_read == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_r);
+		close(fd_w);
+		exit(EXIT_FAILURE);
+	}
+
+	if (close(fd_r) == -1 || close(fd_w) == -1)
+	{
+		perror("Error: Can't close file descriptor");
+		exit(EXIT_FAILURE);
+	}
+
+	return (EXIT_SUCCESS);
+}
