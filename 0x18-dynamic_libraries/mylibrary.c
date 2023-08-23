38f1017aeb8b72151bad46b572f570b1771146b8
Rexbase <gdnwstomw@gmail.com>
2023-08-23 05:29:57 -0700

    Updated

---
+++ 0x18-dynamic_libraries/mylibrary.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// A simple C function that adds two numbers
+int add(int a, int b) {
+    return a + b;
+}
+
+// Another C function that subtracts two numbers
+int subtract(int a, int b) {
+    return a - b;
+}
